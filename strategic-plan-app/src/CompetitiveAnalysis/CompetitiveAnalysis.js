3f7ede5a7e5b6962e5194737774fd60af28e556e
Mike Bell <michaelbell.va@gmail.com>
2018-10-17 15:51:36 -0400

    added json

---
+++ strategic-plan-app/src/CompetitiveAnalysis/CompetitiveAnalysis.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CompetitiveAnalysis extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default CompetitiveAnalysis;
